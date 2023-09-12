e9f7f77334b9d3f8bd5b50f73d27715ce0ad028c
Chingiz <rajablcx@dukes.jmu.edu>
2023-09-11 20:53:18 -0400

    code works now

--- gridTiling/grid_tiling_w3.py
+++ gridTiling/grid_tiling_w3.py
@@ -1,58 +1,81 @@
-size_of_grid = 0
-b = 0
-a = 0
-cnt = 0
-arr = [[0 for i in range(128)] for j in range(128)]
+import math
+"""
+    name:  Chingiz Rajabli
+
+    Honor Code and Acknowledgments:
+
+            This work complies with the JMU Honor Code.
+            - I used this article by geeksforgeeks to understnad the problem a bit better:
+            https://www.geeksforgeeks.org/tiling-problem-using-divide-and-conquer-algorithm/
+  
+           Comments here on your code and submission.
+"""
+
+
+def main():
+    grid_size = int(math.pow(2, int(input())))
+    a, b = map(int, input().split())
+    global tile_num
+    tile_num = 0
+    grid = [[0 for _ in range(grid_size)] for _ in range(grid_size)]
+    grid[a][b] = -1
+    tile(grid_size, 0, 0, grid)
  
-def place(x1, y1, x2, y2, x3, y3):
-    global cnt
-    cnt += 1
-    arr[x1][y1] = cnt;
-    arr[x2][y2] = cnt;
-    arr[x3][y3] = cnt;
+    for i in range(grid_size):
+        for j in range(grid_size):
+            print(f"{grid[i][j]:02d}", end=" ")
+        print()
+
+
+def place(x1, y1, x2, y2, x3, y3, grid):
+    global tile_num
+    tile_num += 1
+    grid[x1][y1] = tile_num
+    grid[x2][y2] = tile_num
+    grid[x3][y3] = tile_num
      
-def tile(n, x, y):
-    global cnt
+def tile(n, x, y, grid):
+    global tile_num
     r = 0
     c = 0
+
     if (n == 2):
-        cnt += 1
+        tile_num += 1
         for i in range(n):
             for j in range(n):
-                if(arr[x + i][y + j] == 0):
-                    arr[x + i][y + j] = cnt
-        return 0;   
+                if(grid[x + i][y + j] == 0):
+                    grid[x + i][y + j] = tile_num
+        return 0   
+    
     for i in range(x, x + n):
         for j in range(y, y + n):
-            if (arr[i][j] != 0):
+            if (grid[i][j] != 0):
                 r = i
                 c = j 
-    if (r < x + n / 2 and c < y + n / 2):
-        place(x + int(n / 2), y + int(n / 2) - 1, x + int(n / 2), y + int(n / 2), x + int(n / 2) - 1, y + int(n / 2))
+
+    if (r < x + n // 2 and c < y + n // 2):
+        place(x + (n // 2), y + (n // 2) - 1, x + (n // 2), 
+              y + (n // 2), x + (n // 2) - 1, y + (n // 2), grid)
      
-    elif(r >= x + int(n / 2) and c < y + int(n / 2)):
-        place(x + int(n / 2) - 1, y + int(n / 2), x + int(n / 2), y + int(n / 2), x + int(n / 2) - 1, y + int(n / 2) - 1)
+    elif(r >= x + (n // 2) and c < y + (n // 2)):
+        place(x + (n // 2) - 1, y + (n // 2), x + (n // 2), 
+              y + (n // 2), x + (n // 2) - 1, y + (n // 2) - 1, grid)
      
-    elif(r < x + int(n / 2) and c >= y + int(n / 2)):
-        place(x + int(n / 2), y + int(n / 2) - 1, x + int(n / 2), y + int(n / 2), x + int(n / 2) - 1, y + int(n / 2) - 1)
+    elif(r < x + (n // 2) and c >= y + (n // 2)):
+        place(x + (n // 2), y + (n // 2) - 1, x + (n // 2), 
+              y + (n // 2), x + (n // 2) - 1, y + (n // 2) - 1, grid)
      
-    elif(r >= x + int(n / 2) and c >= y + int(n / 2)):
-        place(x + int(n / 2) - 1, y + int(n / 2), x + int(n / 2), y + int(n / 2) - 1, x + int(n / 2) - 1, y + int(n / 2) - 1)
+    elif(r >= x + (n // 2) and c >= y + (n // 2)):
+        place(x + (n // 2) - 1, y + (n // 2), x + (n // 2), 
+              y + (n // 2) - 1, x + (n // 2) - 1, y + (n // 2) - 1, grid)
      
-    tile(int(n / 2), x, y + int(n / 2));
-    tile(int(n / 2), x, y);
-    tile(int(n / 2), x + int(n / 2), y);
-    tile(int(n / 2), x + int(n / 2), y + int(n / 2));
-     
-    return 0
- 
-size_of_grid = 8
-a = 0
-b = 0
-arr[a][b] = -1
-tile(size_of_grid, 0, 0)
- 
-for i in range(size_of_grid):
-    for j in range(size_of_grid):
-        print(arr[i][j], end=" ")
-    print()
+    tile((n // 2), x, y + (n // 2), grid)
+    tile((n // 2), x, y, grid)
+    tile((n // 2), x + (n // 2), y, grid)
+    tile((n // 2), x + (n // 2), y + (n // 2), grid)
+      
+
+
+if __name__ == "__main__":
+    main()
+
