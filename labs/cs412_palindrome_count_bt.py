d471ec2c98d8fcc56084456c9668e638d0150b1d
Chingiz <rajablcx@dukes.jmu.edu>
2023-09-18 19:35:31 -0400

    final version

--- labs/cs412_palindrome_count_bt.py
+++ labs/cs412_palindrome_count_bt.py
@@ -2,21 +2,21 @@ def main():
     n = int(input())
     for _ in range(n):
         word = input()
-        count_palindrome(word)
+        print(count_palindrome(word))
 
 
 def count_palindrome(word):
     count = 0
-    if (len(word) == 1):
+
+    if (len(word) <= 1):
         return 1
     
     for i in range(len(word)):
-        if is_palindrome(word[:i+1]):
-            count += 1
+        if is_palindrome(word[:i + 1]):
+            count += count_palindrome(word[i + 1:])
     
-    count += count_palindrome(word[1:])
-
     return count
+
 
 def is_palindrome(word):
     return word == word[::-1]
