511e15fa309712d8254dcecde407410d74b5c1d6
crajabli <rajablcx@dukes.jmu.edu>
2023-11-01 15:35:00 -0400

    added Dijskstra

--- homework/cs412_railroad_a.py
+++ homework/cs412_railroad_a.py
@@ -1,14 +1,8 @@
+from connected_components import count_and_label
+
+
 def railroad():
     pass
-
-def dfs_label(graph, v, labels, currentLabel):
-    bag = [v]
-    while bag: # while bag is not empty
-        u = bag.pop()
-        if labels[u] == -1:
-            labels[u] = currentLabel
-            for w in graph[u]:
-                bag.append(w)
 
 
 def calculate_distance(city1, city2):
